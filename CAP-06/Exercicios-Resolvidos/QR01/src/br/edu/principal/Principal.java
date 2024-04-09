9ffbedab72641806dc76723e986ceed9b5a9df3c
João Gabriel Aguiar  de Senna <134979228+Jot4g3@users.noreply.github.com>
2024-04-09 15:25:33 -0300

    QR01

---
+++ CAP-06/Exercicios-Resolvidos/QR01/src/br/edu/principal/Principal.java
@@ -0,0 +1,35 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int num[];
+		num = new int[9];
+		Scanner sc = new Scanner (System.in);
+		int i, j, cont;
+		for (i=0; i<9; i++) {
+			System.out.println("Digite um número.");
+			num[i]=sc.nextInt();
+		}
+		for (i=0; i<9; i++) {
+			cont = 0;
+			for (j=1; j<=num[i];j++) {
+				if (num[i]%j == 0) {
+					cont+=1;
+				}
+			}
+			if (cont<=2) {
+				if (num[i]==1) {
+					continue;
+				}
+				else {
+					System.out.println("O número é: "+num[i]);
+					System.out.println("A posição do número é :"+ i);	
+				}
+			}
+		}
+	}
+
+}
