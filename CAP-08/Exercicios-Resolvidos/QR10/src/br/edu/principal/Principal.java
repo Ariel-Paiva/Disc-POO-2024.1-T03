4383406d5ee93c38eca6e89ac34fe62ac3868174
Kauã <67508260+kkauaon@users.noreply.github.com>
2024-04-16 12:38:13 -0300

    Questão resolvida 10

---
+++ CAP-08/Exercicios-Resolvidos/QR10/src/br/edu/principal/Principal.java
@@ -0,0 +1,37 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+	static Scanner sc = new Scanner(System.in);
+	
+	public static double sequencia(int n) {
+		int a, b, f;
+		double seq;
+		
+		seq = 1;
+		
+		for (a = 1; a <= n; a++) {
+			f = 1;
+			for (b = 1; b <= a; b++) {
+				f *= b;
+			}
+			
+			seq += 1. / f;
+		}
+		
+		return seq;
+	}
+	
+	public static void main(String[] args) {
+		int num;
+		double s;
+		
+		System.out.println("Informe um número inteiro e positivo: ");
+		num = sc.nextInt();
+		
+		s = sequencia(num);
+		System.out.println("Valor da sequência: " + s);
+	}
+
+}
